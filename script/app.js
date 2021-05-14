6ec5cd6a25d85984a39f67d47a55c3ae0048babe
nayyyhaa <nehaguptang.ng@gmail.com>
2021-05-14 18:05:14 +0530

    feat: filterOption

--- script/app.js
+++ script/app.js
@@ -2,10 +2,12 @@
 let todoInput = document.querySelector(".todo-input"); //alt + shift + down for multiple copies
 let todoSubmit = document.querySelector(".todo-submit");
 let todoList = document.querySelector(".todo-list");
+let filterTodos = document.querySelector(".filter-todos")
 
 //Event Listeners
 todoSubmit.addEventListener("click",addTodoHandler);
 todoList.addEventListener("click",deleteCheckHandler);
+filterTodos.addEventListener("click",filterTodosHandler);
 
 //Functions
 function addTodoHandler(event){
@@ -24,6 +26,8 @@ function addTodoHandler(event){
 }
 
 function addTodo(inputValue){
+    //Create todo div
+    const div = document.createElement("div");
     let li=document.createElement("li");
     li.appendChild(document.createTextNode(inputValue));
     li.innerHTML=`  
@@ -31,14 +35,16 @@ function addTodo(inputValue){
                     <button class="todo-delete"><i class="fa fa-trash-o"></i></button>
                     <button class="todo-check"><i class="fa fa-check-circle"></i></button>
                 `
-    todoList.appendChild(li);
+    div.classList.add("todo");
+    div.appendChild(li)
+    todoList.appendChild(div);
     todoInput.value="";
 }
 
 function deleteCheckHandler(e){
     //getting clicked target 
     let item = e.target;
-    //console.log(item); - whatever clicked(button,div)
+    //console.log(item); - whatever clicked(button,li)
     //whole <li>
     let todo=e.target.parentElement;
     if(item.classList[0]==="todo-delete"){
@@ -56,4 +62,34 @@ function deleteCheckHandler(e){
         todo.classList.add("slash");
         }
     }
+}
+
+function filterTodosHandler(e){
+    //option value - all, completed
+    let optionValue = e.target.value;
+    let todos = Array.from(document.querySelectorAll('.todo-list>div'));
+    //console.log(todos) //div's array
+    todos.forEach(todoss => {
+        let todo = todoss.querySelector('li');
+        //console.log(todo)
+        //console.log(todo.classList)
+        switch(optionValue){
+            case 'all' : 
+                todo.style.display = "flex";break;
+
+            case 'completed' : 
+                if (todo.classList.contains("slash")) {
+                todo.style.display = "flex";
+                } else {
+                todo.style.display = "none";
+                }break;
+
+            case 'uncompleted' : 
+                if (!todo.classList.contains("slash")) {
+                todo.style.display = "flex";
+                } else {
+                todo.style.display = "none";
+                }break;
+        }
+    })
 }
