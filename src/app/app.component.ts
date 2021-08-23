2e974253572e64a1c5acb0dab402e0c9b4059e9c
nayyyhaa <nehaguptang.ng@gmail.com>
2021-08-23 18:29:19 +0530

    add: angular-todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-todo';
+  todo: string ="";
+  todos: string[] = [];
+
+  addTodo(todo: string) {
+    this.todos.push(todo);
+    console.log(this.todos)
+  }
+   
 }
